e769c43acdea8b5028ccc1587b4e22d580f54348
Jake Sanghavi <jake.sanghavi@gmail.com>
2023-09-11 15:14:19 -0400

    first commit

---
+++ index.tsx
@@ -0,0 +1,35 @@
+import { Elysia } from "elysia";
+import { html } from "@elysiajs/html";
+import * as elements from "typed-html";
+
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) => html(
+      <BaseHtml>
+        <body>
+          <h1>Hello World!</h1>
+        </body>
+      </BaseHtml>
+    )
+  )
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
+
+const BaseHtml =({ children }: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>THE BETH STACK</title>
+  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+  <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
+</head>
+${children}
+`;
+
+//  <link href="/styles.css" rel="stylesheet">
